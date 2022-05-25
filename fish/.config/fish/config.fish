4c21fe2222762f595ca52027de58e94f2c343f73
rbnrayan <rayan.robion7@gmail.com>
2022-05-25 13:42:21 +0200

    v0.1 dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    fish_vi_key_bindings
+end
+
+set LS_COLORS $LS_COLORS:'ow=01;34:'
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/rayan/.ghcup/bin $PATH # ghcup-env
+
+fish_add_path /home/rayan/.local/bin
