4c89623f46e58abd8c9bdc0339a7cc6f66990f4c
rbnrayan <rayan.robion7@gmail.com>
2022-05-29 14:32:51 +0200

    fish_alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,5 @@ set LS_COLORS $LS_COLORS:'ow=01;34:'
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/rayan/.ghcup/bin $PATH # ghcup-env
 
 fish_add_path /home/rayan/.local/bin
+
+alias nvim=nvim.appimage
